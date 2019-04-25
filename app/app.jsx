d84319abe11720af831ef05101b083a60edcf288
TAWHID ALI <tawhidali@tawhids-mbp.lan>
2019-04-24 20:44:29 -0400

    updated app

--- app/app.jsx
+++ app/app.jsx
@@ -1,1 +1,13 @@
-console.log('yope');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+let App = (props) => {
+    return(
+        <div>
+            <h1>hi</h1>
+        </div>
+    )
+} 
+
+
+ReactDOM.render(<App/>, document.getElementById('root'));
