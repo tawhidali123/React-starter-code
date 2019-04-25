68d60196131842422118ecc1bb59de003927a4d2
TAWHID ALI <tawhidali@TAWHIDs-MacBook-Pro.local>
2019-04-25 11:42:01 -0400

    included state

--- app/app.jsx
+++ app/app.jsx
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-let App = (props) => {
-    return(
-        <div>
-            <h1>hi</h1>
-        </div>
-    )
-} 
+import SearchBar from './components/searchBar/searchbar.jsx';
+
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchValue: ''
+
+        } 
+
+    }
+
+    handleSearchBarEvent(e) {
+        e.preventDefault();
+
+        let form= e.target;
+        let searchValue = form.searchBarInput.value;
+
+        this.setState({
+            searchValue: searchValue
+        })
+    }
+
+    render() {
+        return(
+            <div>
+                <SearchBar 
+                    handleSearchBarEvent={this.handleSearchBarEvent.bind(this)}
+                />
+                <h1>{this.state.searchValue}</h1>
+    
+            </div>
+        )
+    }
+}
+
+
+
 
 
 ReactDOM.render(<App/>, document.getElementById('root'));
