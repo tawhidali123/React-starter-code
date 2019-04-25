0bfe847d07b69db6e49566076f54172090eeae90
TAWHID ALI <tawhidali@tawhids-mbp.lan>
2019-04-24 20:29:06 -0400

    added webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,20 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'public/bundle'),
         filename: 'dist.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        ]
     }
 
 }
+
